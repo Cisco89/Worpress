536c88281601d1d7770cb0e97b3ed1ac187f3c11
Cisco <franciscoadriangarcia1@gmail.com>
2017-05-22 23:42:25 -0600

    Barebones for Wordpress theme

---
+++ wp-content/themes/lol_friend_compare/footer.php
@@ -0,0 +1,9 @@
+<footer class="site-footer">
+
+    <p><?= bloginfo('name'); ?> - &copy; <?= date('Y'); ?></p>
+
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp-content/themes/lol_friend_compare/index.php
@@ -0,0 +1,7 @@
+<?php
+
+get_header();
+
+echo '<p>No content found</p>';
+
+get_footer();
